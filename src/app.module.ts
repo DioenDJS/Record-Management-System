1323f44f9afc151dfd29fcc96108a44f6934e77a
DioenDJS <negociospacheti@gmail.com>
2021-11-27 14:26:13 -0300

    :art: add config strategy code first

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [GraphQLModule.forRoot({}),],
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  }),],
   providers: [AppService],
 })
 export class AppModule {}

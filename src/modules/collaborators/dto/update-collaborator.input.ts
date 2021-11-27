e4c41e04b7a22bdcb56460652db6ef4d99352436
DioenDJS <negociospacheti@gmail.com>
2021-11-27 17:05:55 -0300

    :art: create file UpdateCollaboratorInput

---
+++ src/modules/collaborators/dto/update-collaborator.input.ts
@@ -0,0 +1,8 @@
+import { CreateCollaboratorInput } from './create-collaborator.input';
+import { InputType, Field, ID, PartialType } from '@nestjs/graphql';
+
+@InputType()
+export class UpdateCollaboratorInput extends PartialType(CreateCollaboratorInput) {
+  @Field(() => ID)
+  id?: string;
+}

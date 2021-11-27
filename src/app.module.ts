b21433286ae368e1953fed07522d078670eb9eff
DioenDJS <negociospacheti@gmail.com>
2021-11-27 14:14:49 -0300

    :art: import module GraphQL forRoot in module app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot({}),],
   providers: [AppService],
 })
 export class AppModule {}

b6d27453e962f356f179f58adcc9bb23851e5855
DioenDJS <negociospacheti@gmail.com>
2021-11-27 18:14:48 -0300

    :recycle: add field information base entity in file DTO

--- src/modules/collaborators/dto/collaborator.dto.ts
+++ src/modules/collaborators/dto/collaborator.dto.ts
@@ -1,13 +1,25 @@
 import { FilterableField } from "@nestjs-query/query-graphql";
-import { ObjectType } from "@nestjs/graphql";
+import { ID, ObjectType } from "@nestjs/graphql";
 
 @ObjectType('Collaborator')
 export class CollaboratorDTO {
+
+  @FilterableField(() => ID)
+  id?: string;
 
   @FilterableField()
   name: string;
   
   @FilterableField()
   CPF: string;
+
+  @FilterableField()
+  createdAt?: Date;
+
+  @FilterableField()
+  updatedAt?: Date;
+
+  @FilterableField()
+  deletedAt?: Date;
   
 }

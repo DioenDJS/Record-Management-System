73636f3e99163a6d29b7a298915a4a0d3ce54a09
DioenDJS <negociospacheti@gmail.com>
2021-11-27 18:16:40 -0300

    :recycle: add field information base entity

--- src/modules/collaborators/entities/collaborator.entity.ts
+++ src/modules/collaborators/entities/collaborator.entity.ts
@@ -1,11 +1,23 @@
-import { Column, Entity } from 'typeorm';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class Collaborator {
 
+  @PrimaryGeneratedColumn('uuid')
+  id?: string;
+
   @Column()
   name: string;
-
+  
   @Column({unique:true})
   CPF: string;
+  
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt?: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt?: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt?: Date;
 }

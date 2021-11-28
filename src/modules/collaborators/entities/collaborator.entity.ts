07da82405b14fa1e46bef0a381ffd75e1b7f31f0
DioenDJS <negociospacheti@gmail.com>
2021-11-28 14:01:26 -0300

    :recycle: refactoring entity removing basic information

--- src/modules/collaborators/entities/collaborator.entity.ts
+++ src/modules/collaborators/entities/collaborator.entity.ts
@@ -1,23 +1,13 @@
+import { BaseEntity } from 'src/modules/base/entities/base.entity';
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
-export class Collaborator {
-
-  @PrimaryGeneratedColumn('uuid')
-  id?: string;
+export class Collaborator extends BaseEntity {
 
   @Column()
   name: string;
   
   @Column({unique:true})
   CPF: string;
-  
-  @CreateDateColumn({ name: 'created_at' })
-  createdAt?: Date;
 
-  @UpdateDateColumn({ name: 'updated_at' })
-  updatedAt?: Date;
-
-  @DeleteDateColumn({ name: 'deleted_at' })
-  deletedAt?: Date;
 }

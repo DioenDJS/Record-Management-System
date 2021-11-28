15f2c85150e30cc6323878d64c45a975a89a2381
DioenDJS <negociospacheti@gmail.com>
2021-11-28 14:29:06 -0300

    :art: add import CollaboratorsService

--- src/modules/collaborators/collaborators.module.ts
+++ src/modules/collaborators/collaborators.module.ts
@@ -1,6 +1,7 @@
 import { NestjsQueryGraphQLModule, PagingStrategies } from '@nestjs-query/query-graphql';
 import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
 import { Module } from '@nestjs/common';
+import { CollaboratorsService } from './collaborators.service';
 import { CollaboratorDTO } from './dto/collaborator.dto';
 import { CreateCollaboratorInput } from './dto/create-collaborator.input';
 import { UpdateCollaboratorInput } from './dto/update-collaborator.input';
@@ -10,18 +11,20 @@ import { Collaborator } from './entities/collaborator.entity';
   imports:[
     NestjsQueryGraphQLModule.forFeature({
       imports: [NestjsQueryTypeOrmModule.forFeature([Collaborator])],
+      services:[CollaboratorsService],
       resolvers: [
         { 
           DTOClass: CollaboratorDTO, 
           EntityClass: Collaborator,
           CreateDTOClass: CreateCollaboratorInput,
           UpdateDTOClass: UpdateCollaboratorInput,
+          ServiceClass: CollaboratorsService,
           enableTotalCount: true,
           pagingStrategy: PagingStrategies.OFFSET,
         }
       ],
     }),
   ],
-  providers: []
+  providers: [CollaboratorsService]
 })
 export class CollaboratorsModule {}

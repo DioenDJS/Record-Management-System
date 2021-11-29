6c9c0c6ae0819cca05d04041060ee3fc400ddef7
DioenDJS <negociospacheti@gmail.com>
2021-11-29 00:23:25 -0300

    :art: creates relationship between collaborator/task

--- src/modules/collaborators/dto/collaborator.dto.ts
+++ src/modules/collaborators/dto/collaborator.dto.ts
@@ -1,8 +1,10 @@
-import { FilterableField } from "@nestjs-query/query-graphql";
+import { FilterableField, FilterableOffsetConnection } from "@nestjs-query/query-graphql";
 import { ObjectType } from "@nestjs/graphql";
 import { BaseDTO } from "src/modules/base/dto/base.dto";
+import { TaskDTO } from "src/modules/tasks/dto/task.dto";
 
 @ObjectType('Collaborator')
+@FilterableOffsetConnection('tasks', () => TaskDTO, { nullable: true })
 export class CollaboratorDTO extends BaseDTO{
 
   @FilterableField()

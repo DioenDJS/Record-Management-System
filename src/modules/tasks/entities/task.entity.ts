532cfa2d8c8759f4e0bf7818009432c25114df06
DioenDJS <negociospacheti@gmail.com>
2021-11-29 00:10:36 -0300

    :sparkles: add enums TaskType in Entity

--- src/modules/tasks/entities/task.entity.ts
+++ src/modules/tasks/entities/task.entity.ts
@@ -1,4 +1,9 @@
 import { BaseEntity } from 'src/modules/base/entities/base.entity';
+export enum TaskType {
+  OPEN = "OPEN",
+  IN_PROGRESS = "IN_PROGRESS",
+  FINISHED = "FINISHED",
+}
 @Entity()
 export class Task extends BaseEntity {
 
@@ -11,6 +16,8 @@ export class Task extends BaseEntity {
   @Column()
   deliveryDate: Date;
 
+  @Column({ type: 'enum', enum: TaskType , nullable: true  })
+  taskType?: TaskType;
   @Column({nullable: true})
   collaboratorId?: string;
 
